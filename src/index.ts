fc0ff71ed59f9303889beda8ab7dce5b93a78106
Soyal <345723940@qq.com>
2020-07-19 19:16:28 +0800

    save

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export default function main(jsonObj: object) {
+  if(typeof jsonObj !== 'object') {
+    throw new Error('jsonObj should be a object')
+  }
+
+  const schema = parse
+}
+
---
+++ src/parse.ts
@@ -0,0 +1,8 @@
+/**
+ * 根据jsonObj解析出json schema
+ * @param jsonObj { [key] }
+ * @param name 属性名
+ */
+export default function parse(name: string, jsonObj: { [key:string]: any }): {} {
+
+}
